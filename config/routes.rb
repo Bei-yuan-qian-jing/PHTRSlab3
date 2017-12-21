1c61d08d45e4cf1b487c36b8461fa8204319f54e
Yourui <csyourui@gmail.com>
2017-12-21 23:08:44 +0800

    1
    1

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  post 'sessions/create'
+
+  get 'applicants/new'
+
+  post 'applicants/create'
+
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
